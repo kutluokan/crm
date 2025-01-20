f5d9ffef4a16e84a9928c978688c728a506604d9
Your Name <you@example.com>
2025-01-19 20:28:43 -0600

    a

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -6,7 +6,11 @@ export const auth = {
     const { error } = await supabase.auth.signInWithOAuth({
       provider: 'google',
       options: {
-        redirectTo: window.location.origin
+        redirectTo: `${window.location.origin}`,
+        queryParams: {
+          access_type: 'offline',
+          prompt: 'consent',
+        }
       }
     })
     if (error) throw error
